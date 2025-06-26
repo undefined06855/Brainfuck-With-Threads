15cd43d4f53ce9e74b3e5a2f5561de4906b52797
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2025-06-26 15:07:51 +0100

    log number of params too

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -224,7 +224,7 @@ void bf::Parser::parse() {
                     case 6: ret = syscall(number, params[0], params[1], params[2], params[3], params[4], params[5]);
                 }
 #else
-                std::println("syscall {} not available on platform! params:", number);
+                std::println("syscall {} not available on platform! params ({}):", number, count);
                 for (auto param : params) {
                     std::println("    {}", param);
                 }
